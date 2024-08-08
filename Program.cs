7eed5c38e49d8b1f055bc7650703fd2966bafa9b
Patrick Vink <patrick.vink@ista.nl>
2024-08-08 16:01:50 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using RockPaperScissors;
+
+SimpleGameManager gm = new SimpleGameManager();
+
+do
+{
+    RoundResult result = gm.PlayRound();
+
+    if (result == RoundResult.Player1Win)
+    {
+        Console.WriteLine("Player 1 Wins!");
+    }
+    else if (result == RoundResult.Player2Win)
+    {
+        Console.WriteLine("Player 2 Wins!");
+    }
+    else
+    {
+        Console.WriteLine("It's a draw!");
+    }
+
+    Console.Write("Play again (Y/N)? ");
+} while (Console.ReadLine().ToUpper() == "Y");
